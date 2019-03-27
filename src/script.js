d4ada9c0a720b971307cff1d4de41c2996d2d5ef
Mustafa Aydemir <mustafa@aydemir.im>
2019-03-27 10:55:39 +0300

    parse output

--- src/script.js
+++ src/script.js
@@ -1,6 +1,22 @@
-airscarp.plugin = {
+$(function () {
 
-    init : function(){
-        console.log("Init");
-    }
-};
+    var data = {
+        command: "speedtest-cli"
+    };
+
+    $.post("http://api.airscarp.com:40/server/command/send?ckey=54256cc608a64abd9d046225a0043361&skey=e79bbabf0fbd4c50943e2ff89e2d2c07", data, function(x){
+        var output = x.data;
+        console.log(output);
+        
+        var download_regex = /Download: ([0-9\.]+) Mbit\/s/g;
+        var upload_regex   = /Upload: ([0-9\.]+) Mbit\/s/g;
+    
+        var download = download_regex.exec(output)[1];
+        var upload   = upload_regex.exec(output)[1];
+
+        console.log(download, upload);
+
+        $("#download-speed").text(download);
+        $("#upload-speed").text(upload);
+    });
+});
