cf74f80e939f96d16bc9b8fe0584d969125ceb76
Mustafa Aydemir <mustafa@aydemir.im>
2019-05-19 15:13:45 +0000

    Updated script.js and index.html

--- src/script.js
+++ src/script.js
@@ -1,25 +1,65 @@
 var speedtest = {
     
-    check_speedtest : function () {
-
-        var API_URL = "http://api.airscarp.com:40/server/command/send?ckey=54256cc608a64abd9d046225a0043361&skey=e79bbabf0fbd4c50943e2ff89e2d2c07";
-        var data    = {command: "speedtest-cli"};
+    testing : false,
     
-        $.post(API_URL, data, function(x){
+    parse : function(output){
+        
+        console.log("Parsing...");
+        
+        var download_regex = /Download: ([0-9\.]+) Mbit\/s/g;
+        var upload_regex   = /Upload: ([0-9\.]+) Mbit\/s/g;
+        var ping_regex     = /Hosted by.*?([0-9\.]+) ms\n/g;
+        var from_regex     = /Testing from (.*?).../g;
+        
+        return {
+            "download" : download_regex.exec(output)[1],
+            "upload"   : upload_regex.exec(output)[1],
+            "ping"     : ping_regex.exec(output)[1],
+            "from"     : from_regex.exec(output)[1],
+        };
+    },
+    
+    show_results : function(data){
+        
+        console.log("Showing results...");
+        
+        $("#download-speed").text(data.download);
+        $("#upload-speed").text(data.upload);
+        $("#ping-speed").text(data.ping);
+        $("#testing-from").text(data.from);
+    },
+    
+    
+    test : function () {
+        
+        if(!speedtest.testing){
+        
+            console.log("Testing...");    
+            speedtest.testing = true;
+    
+            airscarp.plugin.exec("speedtest-cli", function(x){
+                
+                try {
+                    if(!x.s) throw "Something went wrong.";
+                    
+                    // Parse
+                    var results = speedtest.parse(x.output);
             
-            var output = x.data;
-            console.log(output);
-            
-            var download_regex = /Download: ([0-9\.]+) Mbit\/s/g;
-            var upload_regex   = /Upload: ([0-9\.]+) Mbit\/s/g;
+                    // Show
+                    speedtest.show_results(results);
+                    
+                    airscarp.success("Successful!");
+                    
+                } catch (err) {
+                    airscarp.error("Something went wrong.");
+                }
+                
+                speedtest.testing = false;
+            });
+        }
+    },
     
-            var download = download_regex.exec(output)[1];
-            var upload   = upload_regex.exec(output)[1];
     
-            console.log(download, upload);
-    
-            $("#download-speed").text(download);
-            $("#upload-speed").text(upload);
-        });
-    }
+    save : function(){},
 };
+
